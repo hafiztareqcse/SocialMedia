f14c4839a3d2f4ad6d028527f9fef967837454d6
hafiztareqcse <hafiztareqcse@gmail.com>
2021-03-06 15:20:10 +0600

    EditProfile

--- App_Login/models.py
+++ App_Login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_name')
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    dob = models.DateField(blank=True, null=True)
+    facebook = models.URLField(blank=True)
+    website = models.URLField(blank=True)
