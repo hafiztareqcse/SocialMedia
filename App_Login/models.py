8f92e598d3cf401fa751e6e458ce09bc6b7a4309
hafiztareqcse <hafiztareqcse@gmail.com>
2021-03-06 20:32:16 +0600

    Profile Create

--- App_Login/models.py
+++ App_Login/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_name')
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    full_name = models.CharField(max_length=264, blank=True)
     dob = models.DateField(blank=True, null=True)
     facebook = models.URLField(blank=True)
     website = models.URLField(blank=True)
--- App_Login/views.py
+++ App_Login/views.py
@@ -20,7 +20,7 @@ def sign_up(request):
             user_profile = UserProfile(user=user)
             user_profile.save()
             return HttpResponseRedirect(reverse('App_Login:login'))
-    return render(request, 'App_Login/signup.html', context={'title': 'Sign up. LinkedIn', 'form': form})
+    return render(request, 'App_Login/signup.html', context={'title': 'Sign up. Instagram', 'form': form})
 
 
 def login_page(request):
@@ -46,9 +46,16 @@ def edit_profile(request):
         if form.is_valid():
             form.save(commit=True)
             form = EditProfile(instance=current_user)
+            return HttpResponseRedirect(reverse('App_Login:profile'))
     return render(request, 'App_Login/profile.html', context={'form': form, 'title': 'Edit Profile'})
+
 
 @login_required
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('App_Login:login'))
+
+
+@login_required
+def profile(request):
+    return render(request, 'App_Login/user.html', context={'title': 'Profile'})
